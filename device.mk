57ed551b348e944908a695c7d47c84a1a9a37b54
Isaac Chen <tingyi364@gmail.com>
2021-04-24 16:53:34 +0800

    OP4A79: Build audio stack from source
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- device.mk
+++ device.mk
@@ -114,6 +114,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
     libqcompostprocbundle \
@@ -126,15 +127,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info_19065.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_19065.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_19066.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_19066.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
-    $(LOCAL_PATH)/audio/mixer_paths_19065.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_19065.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_19066.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_19066.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_19065.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_19065.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_19066.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_19066.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
